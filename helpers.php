c928b2df9398837ade92a21891e8de5e2c64b81c
mehmetalidurusoy <153413232+mehmetalidurusoy@users.noreply.github.com>
2023-12-17 11:46:53 +0300

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -1,1 +1,21 @@
 <?php
+if(!function_exists("base_url")){
+ function base_url($url = null){
+  return URL::base().$url;
+ }
+}
+if(!function_exists("site_url")){
+ function site_url($url = null){
+    return URL::base().request_uri(REAL_BASE_DIR).'/'.$url;
+ }
+}
+if(!function_exists("public_url")){
+ function public_url($url=null){
+   return site_url().($_SERVER['SERVER_PORT']==80 ? GET_DIRS["DIRECTORY_ROOT"] : '').$url;
+ }
+}
+if(!function_exists("route")){
+ function route($name,$params=[]){
+  return \OceanWT\Http\Route::url($name,$params);
+ }
+}
